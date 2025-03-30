8ad343dffc1ee48ce2e5d306d110fdb7ed408297
Abhijith-02 <69385258+tron01@users.noreply.github.com>
2025-03-30 23:04:18 +0530

    key issue

--- src/components/TopicToMCQ.jsx
+++ src/components/TopicToMCQ.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import axios from "axios";
 const GEMINI_API_URL = "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent";
-const GEMINI_API_KEY = import.meta.env.VITE_GEMINI_API_KEY;
+const GEMINI_API_KEY = process.env.VITE_GEMINI_API_KEY;
 import { CSVLink } from "react-csv";
 
 const TopicToMCQ = () => {
