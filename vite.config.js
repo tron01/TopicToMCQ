1d1689ae9bc1315a3f22c6c5a76fe4fde5743333
Abhijith-02 <69385258+tron01@users.noreply.github.com>
2025-03-30 21:24:40 +0530

    base path issue

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-
 export default defineConfig({
-  base: '/TopicToMCQ/',
+  base: './',
   plugins: [react()],
 });
 
