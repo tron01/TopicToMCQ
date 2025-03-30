60c54034b03a4c5aa6981c1605a5e77e1a3c002a
Abhijith-02 <69385258+tron01@users.noreply.github.com>
2025-03-30 21:00:12 +0530

    config changes

--- src/components/TopicToMCQ.jsx
+++ src/components/TopicToMCQ.jsx
@@ -1,7 +1,8 @@
 import { useState } from "react";
 import axios from "axios";
-import { GEMINI_API_KEY, GEMINI_API_URL } from "../config";
+import {  GEMINI_API_URL } from "../config";
 import { CSVLink } from "react-csv";
+const GEMINI_API_KEY = import.meta.env.VITE_GOOGLE_API_KEY;
 
 const TopicToMCQ = () => {
     const [topic, setTopic] = useState("");
--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,8 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  define: {
+    'process.env.VITE_GOOGLE_API_KEY': `"${process.env.VITE_GOOGLE_API_KEY}"`,
+  },
   plugins: [react()],
 })
